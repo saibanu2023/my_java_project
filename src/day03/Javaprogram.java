4eedf12b891a5fd6085617a3d8b376321f757c28
saibanu katlakunta <123714004+saibanu2023@users.noreply.github.com>
2023-02-08 10:49:29 +0530

    day03
    Java program basic

--- src/day03/Javaprogram.java
+++ src/day03/Javaprogram.java
@@ -4,7 +4,40 @@ public class Javaprogram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Javaprogram xyz=new Javaprogram();
+		addMethod(20,30);
+		xyz.subMethod(10,5);
+		xyz.mulMethod(10,10);
+		
+		addMethod(120,230);
+		xyz.subMethod(110,15);
+		xyz.mulMethod(10,6);
 	}
-
+	public static void addMethod(int a,int b)
+	{
+		System.out.println(a+b);
+		
+	}
+	public void subMethod(int a,int b)
+	{
+		System.out.println(a-b);
+		
+	}
+	public void mulMethod(int a,int b)
+	{
+		System.out.println(a*b);
+	}
 }
+	
+	
+	
+		
+		
+		
+		
+		
+		
+		
+	
+
+
