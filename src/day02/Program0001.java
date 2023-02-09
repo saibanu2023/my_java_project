3a99de74726ce0acb3fed97aef9e2d63c27b6bc9
saibanu katlakunta <123714004+saibanu2023@users.noreply.github.com>
2023-02-09 16:20:01 +0530

    day02 Program0001
    Java program

---
+++ src/day02/Program0001.java
@@ -0,0 +1,33 @@
+package day02;
+
+public class Program0001 {
+
+	public static void fun()
+	{
+		int x=101234567;
+		char y='a';
+				boolean a=true;
+		byte b=127;
+		short c=32000;
+		long d=1234567890;
+		double a1;
+		a1=1234567d;
+		float a2=23456.67f;
+		System.out.println(a2);
+		System.out.println(a1);
+		System.out.println(d);
+		System.out.println("Good Morning....!");
+		System.out.println(x*x);
+	}
+	public static void main(String[]args) {
+		System.out.println("Good");
+		System.out.println("day");
+		fun();
+	}
+
+		
+		// TODO Auto-generated method stub
+
+	}
+
+
