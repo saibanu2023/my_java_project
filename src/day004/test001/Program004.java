68f8d4bdf3c6beedea0e3d26f7ca41010ce3e721
saibanu katlakunta <123714004+saibanu2023@users.noreply.github.com>
2023-02-10 08:44:53 +0530

    day05
    Java Method overloading

---
+++ src/day004/test001/Program004.java
@@ -0,0 +1,10 @@
+package day004.test001;
+
+public class Program004 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/day05/Program01.java
@@ -0,0 +1,32 @@
+package day05;
+
+public class Program01 {
+
+	public static void main(String[] args) 
+	{
+		add(10,20);
+		add(1.1,2.2);
+		add(1,2,3);
+	}
+	public static void add(int a,int b)
+	
+	{
+		System.out.println(a+b);
+	}
+	public static void add(double a,double b)
+	{
+		System.out.println(a+b);
+	}
+	public static void add(int a,int b,int c)
+	{
+		System.out.println(a+b+c);
+	}
+
+
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+
