cc7ac1a72ba632a117aff36a504efe6486573d4d
saibanu katlakunta <123714004+saibanu2023@users.noreply.github.com>
2023-02-13 10:12:07 +0530

    day07

---
+++ src/my_java_project/Program04.java
@@ -0,0 +1,25 @@
+package my_java_project;
+
+public class Program04 {
+
+	public static void main(String[] args) {
+		int a=100;
+		int b=100;
+		System.out.println(" start Program...!");
+		if(a>b)
+		{
+			System.out.println("a is greater...!");
+			
+		}
+		else if(a<b)
+		{
+		System.out.println("a & b are equal...!");
+		
+		}
+		System.out.println("End Program...!");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
