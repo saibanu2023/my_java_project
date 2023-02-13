2972a5d18105d46112a2b7a87b8fba64c685e891
saibanu katlakunta <123714004+saibanu2023@users.noreply.github.com>
2023-02-13 08:41:07 +0530

    day07

---
+++ src/day07/Program01.java
@@ -0,0 +1,21 @@
+package day07;
+
+public class Program01 {
+	
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		System.out.println("Start Program....!");
+		if(a>b)
+		{System.out.println("GoodDay..!");
+		
+		}
+		System.out.println("End Program....!");
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
