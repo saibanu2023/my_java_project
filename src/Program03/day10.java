1a104e2d90c7cd44bc6268b51678e977da0c6922
saibanu katlakunta <123714004+saibanu2023@users.noreply.github.com>
2023-02-21 08:52:46 +0530

    day10
    Program03

---
+++ src/Program03/day10.java
@@ -0,0 +1,32 @@
+package Program03;
+
+public class day10 {
+
+	public static void main(String[] args) {
+		int table=6;
+		System.out.println("----While Program----");
+		int a=1;
+		while(a<=10)
+		{
+			System.out.println(table+"x"+a+"="+table*a);
+			a=a+1;
+		}
+		System.out.println("----Do-While Program----");
+		int b=1;
+		do {
+			System.out.println(table+"x"+b+"="+table*b);
+			b=b+1;
+		}while(b<=10);
+		System.out.println("----For Program----");
+		for(int c=1;c<=10;c=c+1)
+		{
+			System.out.println(table+"x"+c+"="+table*c);
+		}
+	}
+	
+		
+		// TODO Auto-generated method stub
+
+	}
+
+
