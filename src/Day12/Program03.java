22c916d2475489e93bf11cbab8c4d94e51ca1b91
saibanu katlakunta <123714004+saibanu2023@users.noreply.github.com>
2023-03-07 16:43:25 +0530

    Day12
    Program03

---
+++ src/Day12/Program03.java
@@ -0,0 +1,20 @@
+package Day12;
+
+public class Program03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.println("*\t");
+			}
+			System.out.println();
+		
+			}
+		}
+
+	}
+
+
