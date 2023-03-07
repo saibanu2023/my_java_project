11080d6ce889ff46a7d1d5c422cf113b584cf48f
saibanu katlakunta <123714004+saibanu2023@users.noreply.github.com>
2023-03-07 12:02:20 +0530

    Day11
    Program04

---
+++ src/Day11/program04.java
@@ -0,0 +1,48 @@
+package Day11;
+
+public class program04 {
+
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+	/*
+	 54321
+	 4321
+	 321
+	 21
+	 1
+	 
+	 1
+	 21
+	 321
+	 4321
+	 54321
+	 */
+
+for(int i=5;i>=1;i--)//54321
+	
+{
+for(int j=i;j>=1;j--)
+{
+	System.out.print(j);
+}
+
+System.out.println();
+}
+for(int i=1;i<=5;i++)// 12345
+{
+	for(int j=i;j>=1;j--)
+	{
+		System.out.println(j);
+	}
+	System.out.println();
+}
+
+	}
+	
+		
+
+
+
+	}
+
