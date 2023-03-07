0b7be0a382a18b5fb79d7673e200a826eaac8b0f
saibanu katlakunta <123714004+saibanu2023@users.noreply.github.com>
2023-03-07 11:18:56 +0530

    java
    day 10

---
+++ src/day10/Program10.java
@@ -0,0 +1,10 @@
+package day10;
+
+public class Program10 {
+	
+	public static void main(String[] args) {
+		System.out.println("$$$$$$$$$$$$");
+	}
+	
+
+}
---
+++ src/java/program10.java
@@ -0,0 +1,8 @@
+package java;
+
+public class program10 {
+
+	public static void main(String[] args) {
+		System.out.println("$$$$$$$$$$$$");
+	}
+}
